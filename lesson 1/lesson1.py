5cc709815476401680f574e40a90dd79d8b3cedf
Peter Cheung <peter@quantr.hk>
2026-03-27 18:43:24 +0800

    dev

--- lesson 1/lesson1.py
+++ lesson 1/lesson1.py
@@ -3,7 +3,7 @@ from manim import *
 
 class Test(Scene):
     def construct(self):
-        c = Circle(radius=3, color=BLUE, fill_opacity=0.5)
+        c = Circle(radius=3.5, color=BLUE, fill_opacity=0.5)
         self.play(DrawBorderThenFill(c), run_time=0.5)
 
         chinese_font = "Noto Sans CJK HK"
@@ -11,6 +11,9 @@ class Test(Scene):
         subtitle = Text("科普短片", font=chinese_font, font_size=36, color=WHITE).shift(DOWN * 1)
         self.play(Write(title), run_time=0.5)
         self.play(Write(subtitle), run_time=0.5)
+
+        a=Arc(3.8, TAU*1/4, -TAU*2.6/4,color=BLUE, stroke_width=18)
+        self.play(Create(a))
         self.wait(1)
 
 
